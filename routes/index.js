454ccf2704997d1fdd1b07b3fee5572e3d85c728
ClementKunzi <clement.kunzi@ikmail.com>
2024-12-11 13:15:47 +0100

    changement de la route index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Ignition!");
+  res.send(
+    "API for project OK-Corral, a web project in the HEIG-VD COMEM ArchiOWeb Course"
+  );
 });
 
 export default router;
